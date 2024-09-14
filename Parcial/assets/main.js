0229941619a1d22e6b3762a35d11a3368494e3f2
Juan José Castellanos Majano <104451345+jjcaste@users.noreply.github.com>
2024-09-13 23:03:40 -0600

    adding updates css and js

---
+++ Parcial/assets/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', function (event) {
+        
+        event.preventDefault(); 
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const address = document.getElementById('address').value;
+        const paymentMethod = document.querySelector('input[name="pago"]:checked')?.value;
+
+        
+        if (name && email && address && paymentMethod) {
+            
+            console.log('Nombre:', name);
+            console.log('Correo Electrónico:', email);
+            console.log('Dirección:', address);
+            console.log('Método de Pago:', paymentMethod);
+
+            alert(`Gracias ${name} por su compra`);
+
+            form.reset();
+        } else {
+            alert('Por favor, completa todos los campos.');
+        }
+    });
+});
